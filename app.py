256b9ac1d1f69fbc30f6fd89b124ce54fded46ad
sleeping-psystudent <b08207042@g.ntu.edu.tw>
2024-03-27 09:19:50 +0800

    keep revising prompts

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ with open("translation.txt", "r") as file:
     trans_prompt = file.read()
 with open("interaction.txt", "r") as file:
     interact_risk = file.read()
-with open("prompt.txt", "r") as file:
-    text_prompt = file.read()
+with open("evaluation.txt", "r") as file:
+    eval_prompt = file.read()
 
 
 # update global api
@@ -32,9 +32,11 @@ def input_API(api):
     except:
         print("There seems to be something wrong with your Gemini API. Please follow our demonstration in the slide to get a correct one.")
 
+"""
 # clear the conversation
 def reset() -> List:
     return []
+"""
 
 # call the model to generate
 def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> List[Tuple[str, str]]:
@@ -42,8 +44,8 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
     # estimate "交流程度"
     input = f"{article}\n\n{inter}"
     interaction = model.generate_content(
-      input,
-      generation_config=genai.types.GenerationConfig(temperature=0),
+        input,
+        generation_config=genai.types.GenerationConfig(temperature=0),
         safety_settings=[
             {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_NONE",},
             {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_NONE",},
@@ -51,13 +53,12 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(interaction.text)
 
     # generate the summary
     input = f"Level of exchange with Taiwan of <country>: {interaction.text}\n\n{prompt}\n{article}"
     response = model.generate_content(
-      input,
-      generation_config=genai.types.GenerationConfig(temperature=0),
+        input,
+        generation_config=genai.types.GenerationConfig(temperature=0),
         safety_settings=[
           {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_NONE",},
           {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_NONE",},
@@ -67,7 +68,7 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
     )
 
     # translate the article into chinese
-    input = f"{trans}\n{response.text}"
+    input = f"{trans}\n\n{response.text}"
     trans_article = model.generate_content(
       input,
       generation_config=genai.types.GenerationConfig(temperature=0)
@@ -89,14 +90,14 @@ def main():
 
         with gr.Column():
             gr.Markdown("# 疫情分析\n輸入一整篇新聞來製作摘要（不用刪去廣告沒關係）")
-            prompt_textbox = gr.Textbox(label="Prompt", value=text_prompt, visible=False)
+            prompt_textbox = gr.Textbox(label="Prompt", value=eval_prompt, visible=False)
             inter_textbox = gr.Textbox(label="Interaction", value=interact_risk, visible=False)
             trans_textbox = gr.Textbox(label="Translation", value=trans_prompt, visible=False)
             article_textbox = gr.Textbox(label="Article", interactive=True, placeholder="請在此輸入文章", value="")
 
         with gr.Row():
             sent_button = gr.Button(value="Send")
-            reset_button = gr.Button(value="Reset")
+            # reset_button = gr.Button(value="Reset")
 
         with gr.Column():
             chatbot = gr.Chatbot()
@@ -106,7 +107,7 @@ def main():
 
         api_textbox.change(input_API, inputs=[api_textbox])
         sent_button.click(interact_summarization, inputs=[trans_textbox, inter_textbox, prompt_textbox, article_textbox], outputs=[chatbot])
-        reset_button.click(reset, outputs=[chatbot])
+        # reset_button.click(reset, outputs=[chatbot])
         copy_button.click(copy_chatbox_content, inputs=[chatbot])
 
     demo.launch(debug=True)
