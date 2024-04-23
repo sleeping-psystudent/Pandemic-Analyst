5de3c980db09eb2b1db44150c060f1553c2d3152
sleeping-psystudent <b08207042@g.ntu.edu.tw>
2024-04-23 16:07:58 +0800

    revise prompts

--- demo.py
+++ demo.py
@@ -14,8 +14,8 @@ with open("interaction.txt", "r") as file:
     interact_risk = file.read()
 with open("evaluation.txt", "r") as file:
     eval_prompt = file.read()
-# with open("assessment.txt", "r") as file:
-#     assess_prompt = file.read()
+with open("assessment.txt", "r") as file:
+    assess_prompt = file.read()
 
 
 # update global api
@@ -43,9 +43,16 @@ def reset() -> List:
 
 # extract the numbers
 def extract(text):
-    pattern = r': (\d+)'
-    matches = list(map(int, re.findall(pattern, text)))
-    return sum(matches)
+    pattern = r'Assessment Criteria(.*?)Special Circumstances'
+    matches = re.findall(pattern, text, re.DOTALL)
+
+    total_score = 0
+    for match in matches:
+        numbers = re.findall(r': (\d+)', match)
+        for num in numbers:
+            total_score += int(num)
+    
+    return total_score
 
 # call the model to generate
 def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> List[Tuple[str, str]]:
@@ -76,9 +83,19 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
           ]
     )
 
-    """
+    # translate the article into chinese
+    assessment=response.text+"\nRisk Score: "+str(extract(response.text))+"/110"
+    input = f"{trans}\n\n{assessment}"
+    trans_article = model.generate_content(
+      input,
+      generation_config=genai.types.GenerationConfig(temperature=0)
+    )
+
+    return [(assessment, trans_article.text)]
+
+def result_assessment(article:str, assess:str, summary:str) -> List[Tuple[str, str]]:
     # assess the situation by sum up the scores
-    input = f"{assess}\n{response.text}"
+    input = f"{article}\n\n{assess}\n\n{summary[0][0]}"
     assessRISK = model.generate_content(
         input,
         generation_config=genai.types.GenerationConfig(temperature=0),
@@ -89,17 +106,15 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
           {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
           ]
     )
-    """
 
-    # translate the article into chinese
-    assessment=response.text+"\nRisk Score: "+str(extract(response.text))+"/110"
-    input = f"{trans}\n\n{assessment}"
+    # translate
+    input = f"請將下面文章翻譯成通順的臺灣繁體中文\n\n{assessRISK.text}"
     trans_article = model.generate_content(
       input,
       generation_config=genai.types.GenerationConfig(temperature=0)
     )
 
-    return [(assessment, trans_article.text)]
+    return [(assessRISK.text, trans_article.text)]
 
 # copy chatbox content
 def copy_chatbox_content(chatbox_content):
@@ -117,7 +132,7 @@ def main():
             gr.Markdown("# 疫情分析\n輸入一整篇新聞來製作摘要（不用刪去廣告沒關係）")
             prompt_textbox = gr.Textbox(label="Prompt", value=eval_prompt, visible=False)
             inter_textbox = gr.Textbox(label="Interaction", value=interact_risk, visible=False)
-            # assess_textbox = gr.Textbox(label="Assessment", value=assess_prompt, visible=False)
+            assess_textbox = gr.Textbox(label="Assessment", value=assess_prompt, visible=False)
             trans_textbox = gr.Textbox(label="Translation", value=trans_prompt, visible=False)
             article_textbox = gr.Textbox(label="Article", interactive=True, placeholder="請在此輸入文章", value="")
 
@@ -128,12 +143,16 @@ def main():
         with gr.Column():
             chatbot = gr.Chatbot()
         with gr.Row():
+            assess_button = gr.Button(value="Assess")
             copy_button = gr.Button(value="Copy")
+        with gr.Column():
+            result = gr.Chatbot()
 
 
         api_textbox.change(input_API, inputs=[api_textbox])
         sent_button.click(interact_summarization, inputs=[trans_textbox, inter_textbox, prompt_textbox, article_textbox], outputs=[chatbot])
         # reset_button.click(reset, outputs=[chatbot])
+        assess_button.click(result_assessment, inputs=[article_textbox, assess_textbox, chatbot], outputs=[result])
         copy_button.click(copy_chatbox_content, inputs=[chatbot])
 
     demo.launch(debug=True)
