ff8b03d4d256fc1e4a97df146d77ad450a6ec263
sleeping-psystudent <b08207042@g.ntu.edu.tw>
2024-04-30 17:43:22 +0800

    revise revise revise

--- demo.py
+++ demo.py
@@ -35,12 +35,12 @@ def input_API(api):
     except:
         print("There seems to be something wrong with your Gemini API. Please follow our demonstration in the slide to get a correct one.")
 
-"""
-# clear the conversation
-def reset() -> List:
-    return []
-"""
 
+# clear the article input
+def clear() -> List:
+    return None
+
+"""
 # extract the numbers
 def extract(text):
     pattern = r'Assessment Criteria(.*?)Special Circumstances'
@@ -53,21 +53,22 @@ def extract(text):
             total_score += int(num)
     
     return total_score
+"""
 
 # extract country and disease
 def extract_country_disease(text):
-    parts = text.split('/')
+    parts = [part.split('\n') for part in text.split('/')]  
     
-    country = parts[0]
-    disease = parts[1]
+    country = parts[0][0]
+    disease = parts[1][0]
     
     return country, disease
 
 # call the model to generate
-def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> List[Tuple[str, str]]:
+def interact_summarization(n:str, trans:str, inter: str, prompt: str, article: str) -> List[Tuple[str, str]]:
     # find out "country" and "disease"
     problem="""
-    Please identify the main countries mentioned in the article or the countries to which this region belongs, as well as the primary diseases mentioned.
+    Please identify the main countries mentioned in the title of the article or the countries to which this region belongs, as well as the primary diseases mentioned.
     Please output in the format "Country/Disease".
     """
     input = f"{article}\n\n{problem}"
@@ -82,12 +83,11 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             ]
     )
     country, disease = extract_country_disease(country_disease.text)
-    print(country_disease.text)
-    print(country, disease)
+    #print(country, disease)
 
     # estimate "diagnostic method"
     problem="""
-    Please mark the diagnostic method for the disease, where "Clinical syndrome is diagnostic" represents the lowest score, "A simple laboratory test is diagnostic" is the intermediate score, and "Advanced or prolonged investigation is required for confirmatory diagnosis" represents the highest score on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
+    Please mark the diagnostic method for the disease, where "Clinical syndrome is diagnostic" represents a low score, "A simple laboratory test is diagnostic" is a intermediate score, and "Advanced or prolonged investigation is required for confirmatory diagnosis" represents a high score on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
     """
     input = f"The name of disease: {disease}\n{problem}"
     diagnostic = model.generate_content(
@@ -100,11 +100,11 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(diagnostic.text)
+    #print(diagnostic.text)
 
     # estimate "pathogen type"
     problem="""
-    Please mark the pathogen type of the disease as "Others," "Bacterial," or "Viral," with "Others" being the lowest score and "Viral" being the highest, on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
+    Please mark the pathogen type of the disease as "Others," "Bacterial," or "Viral," with "Others" being a low score and "Viral" being a high score, on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
     """
     input = f"The name of disease: {disease}\n{problem}"
     pathogen = model.generate_content(
@@ -117,11 +117,11 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(pathogen.text)
+    #print(pathogen.text)
 
     # estimate "reservoir type"
     problem="""
-    Please mark the reservoir type of the disease as "Animal," "Environmental," or "Human," with "Animal" being the lowest score and "Human" being the highest, on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
+    Please mark the reservoir type of the disease as "Animal," "Environmental," or "Human," with "Animal" being a low score and "Human" being a high score, on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
     """
     input = f"The name of disease: {disease}\n{problem}"
     reservoir = model.generate_content(
@@ -134,10 +134,10 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(reservoir.text)
+    #print(reservoir.text)
 
     # estimate "basic reproductive number"
-    problem="""Please mark the basic reproductive number of the disease as "<1," "1.0–2.0," or ">2," with "<1" being the lowest score and ">2" being the highest, on a scale of 1 to 10 based on your own judgement.
+    problem="""Please mark the basic reproductive number of the disease as "less than one," "one to two," or "greater than two," with "less than one" receiving a lower numerical score and "greater than two" receiving a higher numerical score, on a scale of 1 to 10 based on your own judgement.
     """
     input = f"The name of disease: {disease}\n{problem}"
     reproductive = model.generate_content(
@@ -150,10 +150,10 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(reproductive.text)
+    #print(reproductive.text)
 
     # estimate "mode of transmission"
-    problem="""Please mark the mode of transmission of the disease as "Vector-borne or other animal-borne," "Foodborne, waterborne, and direct contact," or "Airborne or droplet," with "Vector-borne or other animal-borne" being the lowest score and "Airborne or droplet" being the highest, on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
+    problem="""Please mark the mode of transmission of the disease as "Vector-borne or other animal-borne," "Foodborne, waterborne, and direct contact," or "Airborne or droplet," with "Vector-borne or other animal-borne" being a low score and "Airborne or droplet" being a high score, on a scale of 1 to 10 based on your own judgement. Please provide the numerical score only.
     """
     input = f"The name of disease: {disease}\n{problem}"
     transmission = model.generate_content(
@@ -166,7 +166,7 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(transmission.text)
+    #print(transmission.text)
 
     # estimate "mortality rate"
     problem="""Please mark the mortality rate of the disease on a scale of 1 to 10 based on your own knowledge, with 1 being the lowest and 10 being the highest.
@@ -182,7 +182,7 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(mortality.text)
+    #print(mortality.text)
 
     # estimate "incubation period"
     problem="""Please mark the incubation period of the disease on a scale of 1 to 10 based on your own knowledge, with 1 being the shortest and 10 being the longest.
@@ -198,7 +198,7 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(incubation.text)
+    #print(incubation.text)
 
     # estimate "GDP"
     problem="""Please indicate the GDP of the country on a scale of 1 to 10, with 1 representing the wealthiest and 10 representing the poorest.
@@ -214,7 +214,7 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(GDP.text)
+    #print(GDP.text)
 
     # estimate "population density"
     problem="""Please indicate the population density of the country on a scale of 1 to 10, with 1 representing the sparsest and 10 representing the most crowded.
@@ -230,7 +230,7 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(density.text)
+    #print(density.text)
 
     # estimate "government stability"
     problem="""Please indicate the level of peace and stability of the country on a scale of 1 to 10, with 1 representing the most peaceful and 10 representing the most turbulent.
@@ -246,7 +246,7 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
-    print(stability.text)
+    #print(stability.text)
 
     # estimate "level of exchange"
     input = f"{country}\n{inter}"
@@ -260,12 +260,28 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE",},
             ]
     )
+    #print(interaction.text)
 
     # generate the summary
-    input = f"Level of exchange with Taiwan of <country>: {interaction.text}\n\n{prompt}\n{article}"
+    eleven = f"""
+1. Diagnostic method for {disease}: {diagnostic.text}
+2. Pathogen type of {disease}: {pathogen.text}
+3. Reservoir type of {disease}: {reservoir.text}
+4. Basic reproductive number of {disease}: {reproductive.text}
+5. Mode of transmission of {disease}: {transmission.text}
+6. Mortality rate of {disease}: {mortality.text}
+7. Incubation period of {disease}: {incubation.text}
+8. GDP of {country}: {GDP.text}
+9. Population density of {country}: {density.text}
+10. Level of peace and stability of {country}: {stability.text}
+11. Level of exchange with Taiwan of {country}: {interaction.text}
+"""
+    input = f"""{prompt}\n{article}
+    """
+    print(input)
     response = model.generate_content(
         input,
-        generation_config=genai.types.GenerationConfig(temperature=0),
+        generation_config=genai.types.GenerationConfig(temperature=n),
         safety_settings=[
           {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_NONE",},
           {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_NONE",},
@@ -275,7 +291,8 @@ def interact_summarization(trans:str, inter: str, prompt: str, article: str) -> 
     )
 
     # translate the article into chinese
-    assessment=response.text+"\nRisk Score: "+str(extract(response.text))+"/110"
+    total_score = diagnostic.text+pathogen.text+reservoir.text+reproductive.text+transmission.text+mortality.text+incubation.text+GDP.text+density.text+stability.text+interaction.text
+    assessment=response.text+"\nRisk Score: "+str(total_score)+"/110"
     input = f"{trans}\n\n{assessment}"
     trans_article = model.generate_content(
       input,
@@ -289,7 +306,7 @@ def result_assessment(article:str, assess:str, summary:str) -> List[Tuple[str, s
     input = f"{article}\n\n{assess}\n\n{summary[0][0]}"
     assessRISK = model.generate_content(
         input,
-        generation_config=genai.types.GenerationConfig(temperature=0),
+        generation_config=genai.types.GenerationConfig(temperature=0.3),
         safety_settings=[
           {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_NONE",},
           {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_NONE",},
@@ -326,10 +343,11 @@ def main():
             assess_textbox = gr.Textbox(label="Assessment", value=assess_prompt, visible=False)
             trans_textbox = gr.Textbox(label="Translation", value=trans_prompt, visible=False)
             article_textbox = gr.Textbox(label="Article", interactive=True, placeholder="請在此輸入文章", value="")
+            temperature_slider = gr.Slider(0.0, 1.0, 0.0, step = 0.1, label="Temperature")
 
         with gr.Row():
             sent_button = gr.Button(value="Send")
-            # reset_button = gr.Button(value="Reset")
+            clear_button = gr.Button(value="Clear")
 
         with gr.Column():
             chatbot = gr.Chatbot()
@@ -341,8 +359,8 @@ def main():
 
 
         api_textbox.change(input_API, inputs=[api_textbox])
-        sent_button.click(interact_summarization, inputs=[trans_textbox, inter_textbox, prompt_textbox, article_textbox], outputs=[chatbot])
-        # reset_button.click(reset, outputs=[chatbot])
+        sent_button.click(interact_summarization, inputs=[temperature_slider, trans_textbox, inter_textbox, prompt_textbox, article_textbox], outputs=[chatbot])
+        clear_button.click(clear, outputs=[article_textbox])
         assess_button.click(result_assessment, inputs=[article_textbox, assess_textbox, chatbot], outputs=[result])
         copy_button.click(copy_chatbox_content, inputs=[chatbot])
 
