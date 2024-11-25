03c73da7639d5b2fb3b2e3f221fe34e7603198d6
Lee Tsai-Rung <81673151+sleeping-psystudent@users.noreply.github.com>
2024-11-25 22:58:29 +0800

    Update app.py

--- map_org/app.py
+++ map_org/app.py
@@ -53,6 +53,22 @@ st.markdown(
         width: 100%;
         padding: 0rem 0rem 0rem;
     }
+
+    .st-emotion-cache-0.e1f1d6gn0 {
+    height: 100%;
+    }
+    .st-emotion-cache-1wmy9hl.e1f1d6gn1 {
+        height: 100%;
+    }
+    .st-emotion-cache-1xl908n.e1f1d6gn4 {
+        height: 100%;
+    }
+    .st-emotion-cache-0.elp1w7k0 {
+        height: 100%;
+    }
+    .stDeckGlJsonChart.st-emotion-cache-whppok.e1az0zs51 {
+        height: 100%;
+    }
     
     #deckgl-wrapper {
         box-sizing: content-box !important;
