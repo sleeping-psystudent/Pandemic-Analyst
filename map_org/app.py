c5eaa4fb14b42ce9e3534f753b42bc3d4c8893be
Lee Tsai-Rung <81673151+sleeping-psystudent@users.noreply.github.com>
2024-12-17 15:36:28 +0800

    Update app.py

--- map_org/app.py
+++ map_org/app.py
@@ -35,65 +35,65 @@ st.set_page_config(page_title="全球疫情地圖", page_icon="map_org/ROC_Cente
 
 # 自定義CSS
 st.markdown(
-    """
-    <style>
-    /* 固定地圖容器 */
-    .map-container {
-        position: fixed;
-        height: 100%;
-        left: 0px; /* 側邊欄的寬度 */
-        margin: 0px;
-        padding: 0px;
-        z-index: 2000;
-    }
+#     """
+#     <style>
+#     /* 固定地圖容器 */
+#     .map-container {
+#         position: fixed;
+#         height: 100%;
+#         left: 0px; /* 側邊欄的寬度 */
+#         margin: 0px;
+#         padding: 0px;
+#         z-index: 2000;
+#     }
 
-    .st-emotion-cache-1itdyc2.eczjsme18 {
-        opacity: 0.85;
-    }
+#     .st-emotion-cache-1itdyc2.eczjsme18 {
+#         opacity: 0.85;
+#     }
 
-    .st-emotion-cache-6qob1r.eczjsme11 {
-        overflow: hidden;
-    }
+#     .st-emotion-cache-6qob1r.eczjsme11 {
+#         overflow: hidden;
+#     }
 
-    .st-emotion-cache-1jicfl2 {
-        position: fixed;
-        height: 100%;
-        left: 0px;
-        width: 100%;
-        padding: 0rem 0rem 0rem;
-    }
+#     .st-emotion-cache-1jicfl2 {
+#         position: fixed;
+#         height: 100%;
+#         left: 0px;
+#         width: 100%;
+#         padding: 0rem 0rem 0rem;
+#     }
 
-    .st-emotion-cache-0.e1f1d6gn0 {
-    height: 95vh;
-    }
-    .st-emotion-cache-1wmy9hl.e1f1d6gn1 {
-        height: 95vh;
-    }
-    .st-emotion-cache-0.elp1w7k0 {
-        height: 95vh;
-    }
+#     .st-emotion-cache-0.e1f1d6gn0 {
+#     height: 95vh;
+#     }
+#     .st-emotion-cache-1wmy9hl.e1f1d6gn1 {
+#         height: 95vh;
+#     }
+#     .st-emotion-cache-0.elp1w7k0 {
+#         height: 95vh;
+#     }
     
-    .stDeckGlJsonChart {
-        height: 95vh !important; /* 將高度設置為視窗高度 */
-    }
+#     .stDeckGlJsonChart {
+#         height: 95vh !important; /* 將高度設置為視窗高度 */
+#     }
     
-    #deckgl-wrapper {
-        box-sizing: content-box !important;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 95vh !important;
-        z-index: 10000;
-    }
+#     #deckgl-wrapper {
+#         box-sizing: content-box !important;
+#         position: fixed;
+#         top: 0;
+#         left: 0;
+#         width: 100%;
+#         height: 95vh !important;
+#         z-index: 10000;
+#     }
 
-    .st-emotion-cache-bm2z3a {
-        height: 0px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+#     .st-emotion-cache-bm2z3a {
+#         height: 0px;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True,
+# )
 
 # 連接資料庫
 conn = sqlite3.connect('./map_org/disease_info.db')  
