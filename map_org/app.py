7a75f1c15286aff8d2a5fbe3c7351dde52a4dda8
Lee Tsai-Rung <81673151+sleeping-psystudent@users.noreply.github.com>
2024-12-17 16:18:38 +0800

    Update app.py

--- map_org/app.py
+++ map_org/app.py
@@ -191,7 +191,7 @@ r = pdk.Deck(
               }
             </style>
             <div id='custom-tooltip' class='tooltip'>
-                <h2 style="margin-top: -5px;margin-bottom: 0px;">{emoji} {risk_assessment}</h2>
+                <h2 style="margin-top: -6px;margin-bottom: 0px;">{emoji} {risk_assessment}</h2>
                  <small style="font-size: 10px;">|| 發布日期: {date} ({weeks_ago} 週前)</small><hr style="margin: 6px 0;margin-bottom: 10px;">
                 <div>{summary}</div>
             </div>
