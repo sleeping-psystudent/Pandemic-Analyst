be189fda2a4d2b4a147c5a47af8efcb1e9703cb5
Lee Tsai-Rung <81673151+sleeping-psystudent@users.noreply.github.com>
2024-12-17 15:57:18 +0800

    Update app.py

--- map_org/app.py
+++ map_org/app.py
@@ -83,7 +83,7 @@ st.markdown(
     
     #deckgl-wrapper {
         box-sizing: content-box !important;
-        position: absolute !important;
+        position: fixed !important;
         top: 0;
         left: 0;
         width: 100%;
