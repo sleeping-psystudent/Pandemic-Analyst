eeee94cd395671be8300a94f237df748afe0d0d8
Lee Tsai-Rung <81673151+sleeping-psystudent@users.noreply.github.com>
2024-12-17 15:18:45 +0800

    Update app.py

--- map_org/app.py
+++ map_org/app.py
@@ -197,9 +197,9 @@ r = pdk.Deck(
 )
 
 # 顯示固定地圖
-st.markdown('<div class="map-container">', unsafe_allow_html=True)
+# st.markdown('<div class="map-container">', unsafe_allow_html=True)
 st.pydeck_chart(r)
-st.markdown('</div>', unsafe_allow_html=True)
+# st.markdown('</div>', unsafe_allow_html=True)
 
 
 # with st.expander("疫情資料表"):
