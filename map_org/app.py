e718303d457df905f30290e4dfe0340fc5951fe5
Lee Tsai-Rung <81673151+sleeping-psystudent@users.noreply.github.com>
2024-12-17 16:15:06 +0800

    Update app.py

--- map_org/app.py
+++ map_org/app.py
@@ -187,7 +187,7 @@ r = pdk.Deck(
             <style>
               .small-gap {
                   display: block;
-                  margin-top: 5px;
+                  margin-top: 10px;
               }
             </style>
             <div id='custom-tooltip' class='tooltip'>
