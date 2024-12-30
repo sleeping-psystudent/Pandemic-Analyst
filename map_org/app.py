fddc6584321c7c696d75e72929008ef63c427f2a
Lee Tsai-Rung <81673151+sleeping-psystudent@users.noreply.github.com>
2024-12-30 15:02:53 +0800

    Update app.py

--- map_org/app.py
+++ map_org/app.py
@@ -179,7 +179,7 @@ r = pdk.Deck(
     initial_view_state=view_state,
     tooltip={
         "style":{
-            "bottom": "50px",
+            "bottom": "-5px",
             "left": "-200px",
             "max-width": "400px",
             "font-size": "12px",
